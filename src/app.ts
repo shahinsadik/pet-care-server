8f211ef2621805ba18932320502e6d45804f47c6
Shahin Sadik <shahinsadik@gmail.com>
2024-11-11 22:26:48 +0600

    user provider

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ import globalErrorHandler from './app/middlewares/globalErrorHandler';
 import cookieParser from 'cookie-parser';
 
 const app: Application = express();
-
 // parser
 app.use(express.json());
 app.use(cookieParser());
